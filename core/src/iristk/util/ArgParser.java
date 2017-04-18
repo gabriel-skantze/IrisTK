09c4d6bf9f2e0b8b01e3323fde0da2ddd2be3eec
Filip De Figueiredo <filip@furhatrobotics.com>
2017-04-18 14:06:35 +0200

    ArgParser: addition of verbose names (i.e. --verbose to complement -v)
    -Added verbose argument constructor for optional and required arguments.
    
    -Added helper method for repeated code in addOptionalArgs and
    addRequiredArgs.
    -Verbose names map to their non-verbose names.
    -project.jar rebuilt for ArgParser changes
    -Spelling mistake fixed in HAT

--- core/src/iristk/util/ArgParser.java
+++ core/src/iristk/util/ArgParser.java
@@ -18,33 +18,65 @@ import java.util.List;
 public class ArgParser {
 
 	HashMap<String,Object> args = new HashMap<String,Object>();
+	/**Maps the verbose name of an argument to the simple argument. (--port maps to -p, or --independent to -i) */
 	HashMap<String,String> descs = new HashMap<String,String>();
 	HashMap<String,String> tokens = new HashMap<String,String>();
 	HashMap<String,Class<?>> types = new HashMap<String,Class<?>>();
 	HashMap<String,Boolean> required = new HashMap<String,Boolean>();
 	HashMap<String,Object> defaults = new HashMap<String,Object>();
+	HashMap<String,String> verboseArgList = new HashMap<String,String>(); 
 	ArrayList<String> argList = new ArrayList<String>();
-	
+
 	private boolean allowRestArguments = false;
 	private List<String> restArguments = new ArrayList<>();
 
-	public void addRequiredArg(String name, String desc, String token, Class<?> type) {
+	/** Helper method for default argument setup, used when adding required and optional arguments 
+	 * @param name - argument name, in a short form.
+	 * @param desc - a description of the argument
+	 * @param token - token to be used inside the description, in the form "-name [token] desc"
+	 * @param type - type of the argument, used for parsing the value.
+	 */
+	private void setupArg(String name, String desc, String token, Class<?> type){
 		this.argList.add(name);
 		this.descs.put(name, desc);
 		this.types.put(name, type);
 		this.tokens.put(name, token);
+	}
+	
+	public void addRequiredArg(String name, String desc, String token, Class<?> type) {
+		setupArg(name, desc, token, type);
 		this.required.put(name, true);
 	}
-
+	
 	public void addOptionalArg(String name, String desc, String token, Class<?> type, Object defaultv) {
-		this.argList.add(name);
-		this.descs.put(name, desc);
-		this.types.put(name, type);
-		this.tokens.put(name, token);
+		setupArg(name, desc, token, type);
 		this.required.put(name, false);
 		this.defaults.put(name, defaultv);
 	}
-
+	
+	/**
+	 * Adds a required argument, with a verbose name. 
+	 */
+	public void addRequiredArg(String fullName, String name, String desc, String token, Class<?> type) {
+		addRequiredArg(name, desc, token, type);
+		addVerboseLink(fullName, name);
+	}
+	
+	/**
+	 * Adds an optional argument, with a verbose name. 
+	 */
+	public void addOptionalArg(String fullName, String name, String desc, String token, Class<?> type, Object defaultv) {
+		addOptionalArg(name, desc, token, type, defaultv);
+		addVerboseLink(fullName, name);
+	}
+	
+	public void addVerboseLink(String linkName, String name) throws IllegalArgumentException{
+		if (!argList.contains(name)) {
+			throw new IllegalArgumentException(name+" is not a known argument");
+		}
+		verboseArgList.put(linkName, name);
+	}
+	
 	public void addBooleanArg(String name, String desc) {
 		this.argList.add(name);
 		this.descs.put(name, desc);
@@ -67,22 +99,33 @@ public class ArgParser {
 	public void parse(String[] cmdargs, int pos, int len) {
 		parse(Arrays.copyOfRange(cmdargs, pos, len+pos));
 	}
-	
+
 	public List<String> getRestArguments() {
 		return restArguments;
 	}
-	
+
 	public void allowRestArguments(boolean b) {
 		this.allowRestArguments = b;
 	}
-	
 	public void parse(String[] cmdargs) {
 		try {
 			String name = null;
 			for (int i = 0; i < cmdargs.length; i++) {
 				String value = cmdargs[i];
+				//if verboseArgList.contains(name) then name is verboseArgList.get(name)
 				if (value.startsWith("-")) {
-					name = value.substring(1);
+					if(value.startsWith("--")){
+						String fullname = value.substring(2);
+						if(verboseArgList.containsKey(fullname)) {
+							name = verboseArgList.get(fullname);
+						} else {
+							throw new ParseException("Cannot recognise argument: --"+ name);
+						}
+
+					} else { 
+						name = value.substring(1);
+					}
+
 					if (argList.contains(name)) {
 						if (types.get(name) == Boolean.class) {
 							args.put(name, true);
@@ -123,7 +166,7 @@ public class ArgParser {
 			System.exit(0);
 		}
 	}
-	
+
 	private class ParseException extends Exception {
 		public ParseException(String msg) {
 			super(msg);
